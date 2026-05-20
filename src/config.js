5d9194145938c3c5b17770f856cd70bca711c93e
dsbly1 <dsbly1@cougars.ccis.edu>
2026-05-20 17:33:08 -0500

    Fix API URL and CORS for production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
+export const API_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:8181" 
+  : "https://med-appt-server.up.railway.app";
 console.log("API_URL :", API_URL);
