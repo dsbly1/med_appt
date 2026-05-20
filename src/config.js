ed4f4add265579709e31d6f99aafd8e1a42ee34c
dsbly1 <dsbly1@cougars.ccis.edu>
2026-05-20 17:43:46 -0500

    Update API URL to Render backend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_URL = window.location.hostname === "localhost" 
   ? "http://localhost:8181" 
-  : "https://med-appt-server.up.railway.app";
+  : "https://med-appt.onrender.com";
 console.log("API_URL :", API_URL);
